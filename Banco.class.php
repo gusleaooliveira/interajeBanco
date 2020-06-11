758fe9188d445650c6a0eb9a773d98ca9075bc46
gusleaooliveira <gus.leaono@gmail.com>
2020-06-10 21:34:45 -0300

    alteração feita em:10/06/2020-21:34:45

--- Banco.class.php
+++ Banco.class.php
@@ -48,18 +48,36 @@ class Banco {
       return null;
     }
   }
+  function executa($sql){
+    try{
+      $stmt = $this->conexao->query($sql);
+      echo "<p><strong>Executado!</strong></p>";
+      return $stmt;
+    }catch(PDOException $erro){
+      echo "<p><strong>Mensagem de erro:</strong>".$erro->getMessage()."</p>";
+      return null;
+    }
+  }
 
 }
 
 $banco =  new Banco();
 
-var_dump($banco->executar_sql("INSERT INTO depoimentos(nome, email, depoimento) VALUES (:nome, :email, :depoimento)",
-  array(
-    ":nome" => "Sônia Leão Nogueira",
-    ":email" => "sonia_leao69@gmail.com",
-    ":depoimento" => "Minha casa foi reformada pela empresa, fizeram um ótimo trabalho."
-  )
-));
+// var_dump($banco->executar_sql("INSERT INTO depoimentos(nome, email, depoimento) VALUES (:nome, :email, :depoimento)",
+//   array(
+//     ":nome" => "Sônia Leão Nogueira",
+//     ":email" => "sonia_leao69@gmail.com",
+//     ":depoimento" => "Minha casa foi reformada pela empresa, fizeram um ótimo trabalho."
+//   )
+// ));
+//
+// var_dump($banco->executar_sql("INSERT INTO depoimentos(nome, email, depoimento) VALUES (:nome, :email, :depoimento)",
+//   array(
+//     ":nome" => "Gustavo Leão Nogueira de Oliveira",
+//     ":email" => "gus.leaono@gmail.com",
+//     ":depoimento" => "Gostei muito da empresa. Me ajudou muito, fizeram a minha casa."
+//   )
+// ));
 
 // var_dump($banco->executar_sql("UPDATE depoimentos SET email = :email WHERE id = :id",
 //   array(
@@ -69,6 +87,9 @@ var_dump($banco->executar_sql("INSERT INTO depoimentos(nome, email, depoimento) 
 // ));
 
 // var_dump($banco->executar("DELETE FROM depoimentos WHERE id = :id",1));
+
+
+//var_dump($resp=$banco->executa("SELECT * FROM depoimentos"));
 
 
 
